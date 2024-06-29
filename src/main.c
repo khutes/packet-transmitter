a55030a8823f94ab8449d7ac06b9d94d93eae7a7
khutes <kyle.hutto3@gmail.com>
2024-06-28 21:37:05 -0700

    feat: read user input for network layer

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+
+#define DEFAULT_NETWORK_LAYER 3
+#define MIN_NETWORK_LAYER 1
+#define MAX_NETWORK_LAYER 3
+
 
 char* get_arg_val(char* key, char**arg_list, int len)
 {
@@ -14,7 +21,11 @@ char* get_arg_val(char* key, char**arg_list, int len)
         }
     }
     return val;
-} 
+
+}
+
+
+
 
 
 
@@ -22,6 +33,9 @@ char* get_arg_val(char* key, char**arg_list, int len)
 int main(int argc, char**argv) 
 {
     char* packet_type = NULL;
+    char* network_layer_str = NULL;
+    int network_layer = DEFAULT_NETWORK_LAYER;
+
 
     printf("Starting packet transmitter\n");
     printf("argc: %d\n", argc);
@@ -30,12 +44,26 @@ int main(int argc, char**argv)
         printf("argv[%d]: %s\n", i, argv[i]);
     }
 
+    /* Get all the required arguments */
     packet_type = get_arg_val("--packet-type", argv, argc);
     
     if (packet_type == NULL) {
         printf("Error: Packet type not specified\n");
         return -1;
     }
+
+    /*Get optional non packet attribute arguments */
+    network_layer_str = get_arg_val("--network-layer", argv, argc);
+    if (network_layer_str != NULL) {
+        network_layer = atoi(network_layer_str);
+        if (network_layer < MIN_NETWORK_LAYER || network_layer > MAX_NETWORK_LAYER) {
+            printf("Error: %s is not a valid network layer\n", network_layer_str);
+            return -1;
+        }
+    }
+    printf("network_layer: %d\n", network_layer);
+
+
 
     printf("packet-type: %s\n", packet_type);
 
